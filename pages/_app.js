8967c0bb2c1de580a60b9f03c2be9132f9f10243
Rubens Filipe <rubensfilipe6@gmail.com>
2023-02-21 22:11:21 -0300

    tirando a pasta src

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Layout } from './Components'
-import '../../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../../css/style.css'
-import '../../css/Button.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import '../css/style.css'
+// import '../css/Button.css'
 
 const MyApp = ({Component, pageProps}) =>{
     return(
